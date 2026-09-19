529330e1c7b2bc579f349c6f527543d82c8d66bc
Gui Faccioli <guifacciolic@gmail.com>
2026-09-19 13:29:35 -0300

    refactor(ui): refine landing visual system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { LandingPage } from './components/LandingPage'
 import './styles/global.css'
 import './styles/official-kit.css'
+import './styles/impeccable.css'
 
 export function App() {
   return <LandingPage />
