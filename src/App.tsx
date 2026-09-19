f13acf03cb3e24c0a3069e8183d988ce374ff13f
Gui Faccioli <guifacciolic@gmail.com>
2026-09-19 15:39:32 -0300

    refactor(ui): make desktop composition distinctive

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { LandingPage } from './components/LandingPage'
 import './styles/global.css'
 import './styles/official-kit.css'
 import './styles/impeccable.css'
+import './styles/desktop-direction.css'
 
 export function App() {
   return <LandingPage />
