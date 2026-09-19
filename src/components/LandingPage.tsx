9d67d2e77328079d31ae6251799fe00833c84384
Gui Faccioli <guifacciolic@gmail.com>
2026-09-19 03:48:53 -0300

    chore: scaffold landing app

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,10 @@
+export function LandingPage() {
+  return (
+    <div className="site-shell">
+      <main id="conteudo">
+        <h1>SouJunior</h1>
+        <p>Landing page em construção.</p>
+      </main>
+    </div>
+  )
+}
